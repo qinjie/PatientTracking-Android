5222f34809705a269a5e4a69dbbe228441f6e96a
Nam <namthse03439@fpt.edu.vn>
2016-09-27 14:44:02 +0800

    fix comments #23

--- app/src/main/java/com/example/intern/ptp/Login/LoginActivity.java
+++ app/src/main/java/com/example/intern/ptp/Login/LoginActivity.java
@@ -114,13 +114,8 @@ public class LoginActivity extends Activity {
      * set layout for the activity
      */
     public void setLayout() {
-        //Remove title bar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        //Remove notification bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        //set content view AFTER ABOVE sequence (to avoid crash)
         this.setContentView(R.layout.activity_login);
 
         ButterKnife.bind(this);
